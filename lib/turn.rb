8db8b61fb0134bb22a04b158ecb58a157a0ad2b8
Vincent Tran <vincentuong@yahoo.com>
2021-05-01 21:07:13 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,7 +40,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index, current_player) && display_board(board)
+    move(board, index) && display_board(board)
   else 
     input = gets.strip
   end
